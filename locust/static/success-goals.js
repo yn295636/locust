8b67f7cc8e1dc157697837278e4316ca8b91a3ec
Jonatan Heyman <jonatan@heyman.info>
2011-05-27 14:47:57 +0200

    More work on success goals. Moved success goals form from the start test form to the success goal tab.

--- locust/static/success-goals.js
+++ locust/static/success-goals.js
@@ -1,7 +1,47 @@
 SuccessGoals = {
     init: function() {
-        this.failPercentGoal = 0.10;
+        $(".success_goals form").submit(function(event) {
+            event.preventDefault();
+            document.location = "/?goals=" + $("#goal_fail").val() + ";" + $("#goal_response95").val();
+        });
+        $("#changeGoals").click(function(event) {
+            event.preventDefault();
+            $('.success_goals .settings').toggle();
+            if ($('.success_goals .settings').is(":visible"))
+                $(this).html("- Change Goals");
+            else
+                $(this).html("+ Change Goals");
+        });
+        
+        this.isGoalSet = false;
+        this.failPercentGoal = 0.1;
         this.responseTimeGoal = 500;
+        
+        urlParts = document.location.href.split("?");
+        if (urlParts.length > 1) {
+            queryParts = urlParts[1].split("&");
+            for (var i=0; i < queryParts.length; i++) {
+                var parts = queryParts[i].split("=")
+                if (parts.length > 0 && parts[0] == "goals") {
+                    var settings = parts[1].split(";");
+                    if (settings.length != 2)
+                        break;
+                    this.failPercentGoal = parseFloat(settings[0]);
+                    this.responseTimeGoal = parseInt(settings[1]);
+                    this.isGoalSet = true;
+                }
+            }
+        }
+        
+        $("#goal_fail").val(this.failPercentGoal);
+        $("#goal_response95").val(this.responseTimeGoal);
+        $("#goal_fail_current").html(this.failPercentGoal + "%");
+        $("#goal_response95_current").html(this.responseTimeGoal + " ms");
+        
+        if (this.isGoalSet) {
+            $(".success_goals .settings").hide();
+            $(".success_goals .result").show();
+        }
     },
     
     failPercentElement: $(".success_goals .fail_percent .status"),
@@ -24,7 +64,7 @@ SuccessGoals = {
     
     updateStats: function(report) {
         var failureOk = report.fail_ratio < SuccessGoals.failPercentGoal;
-        SuccessGoals.setElementStatus(SuccessGoals.failPercentElement, failureOk);
+        SuccessGoals.setElementStatus(SuccessGoals.failPercentElement, failureOk/100);
         SuccessGoals.failPercentValue.html(Math.round(report.fail_ratio*100000)/1000 + " %");
         
         var responseTimeOk = report.response_time_percentile_95 < SuccessGoals.responseTimeGoal;
