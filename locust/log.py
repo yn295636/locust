4194c0f36a3c289520e036000c27c3d09be24eed
yining <toyining@gmail.com>
2019-03-28 14:28:24 +0800

    fix bug

--- locust/log.py
+++ locust/log.py
@@ -4,6 +4,7 @@ import sys
 
 host = socket.gethostname()
 
+
 def setup_logging(loglevel, logfile):
     numeric_level = getattr(logging, loglevel.upper(), None)
     if numeric_level is None:
@@ -11,25 +12,29 @@ def setup_logging(loglevel, logfile):
 
     log_format = "[%(asctime)s] {0}/%(levelname)s/%(name)s: %(message)s".format(host)
     formatter = logging.Formatter(fmt=log_format)
-    file_handler = logging.FileHandler(logfile)
+    handlers = []
+    if logfile is not None:
+        file_handler = logging.FileHandler(logfile)
+        file_handler.setFormatter(formatter)
+        handlers.append(file_handler)
     stream_handler = logging.StreamHandler()
-    file_handler.setFormatter(formatter)
     stream_handler.setFormatter(formatter)
-    logging.basicConfig(level=numeric_level, handlers=[
-        file_handler,
-        stream_handler
-    ])
+    handlers.append(stream_handler)
+    logging.basicConfig(level=numeric_level, handlers=handlers)
 
     sys.stderr = StdErrWrapper()
     sys.stdout = StdOutWrapper()
 
+
 stdout_logger = logging.getLogger("stdout")
 stderr_logger = logging.getLogger("stderr")
+
 
 class StdOutWrapper(object):
     """
     Wrapper for stdout
     """
+
     def write(self, s):
         stdout_logger.info(s.strip())
 
@@ -37,16 +42,19 @@ class StdOutWrapper(object):
         """No-op for wrapper"""
         pass
 
+
 class StdErrWrapper(object):
     """
     Wrapper for stderr
     """
+
     def write(self, s):
         stderr_logger.error(s.strip())
 
     def flush(self, *args, **kwargs):
         """No-op for wrapper"""
         pass
+
 
 # set up logger for the statistics tables
 console_logger = logging.getLogger("console_logger")
